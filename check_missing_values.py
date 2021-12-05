5d0c960f71535205c3626972d2e64fc2ed547ff0
Dima Gromyko <grom.dima.grom@gmail.com>
2021-12-05 21:35:50 +0300

    Update check_missing_values.py

--- check_missing_values.py
+++ check_missing_values.py
@@ -1,15 +1,16 @@
-# That's a feature from Vlad Yashin
-
-def check_missing_values(dataframe):
-    if dataframe.isnull().sum().sum() > 0:
+def check_missing_values_and_duplicates(dataframe):
+    if dataframe.isnull().sum().sum() > 0 or dataframe.isnull().sum().sum() > 0:
         m_total = dataframe.isnull().sum().sort_values(ascending=False)
         total = m_total[m_total > 0]
 
         m_percent = dataframe.isnull().mean().sort_values(ascending=False)
         percent = m_percent[m_percent > 0]
 
-        missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+        m_duplicates = dataframe.duplicated().sort_values(ascending=False)
+        duplicates = m_duplicates[m_duplicates > 0]
 
-        print(f'Total and Percentage of NaN:\n {missing_data}')
+        missing_data = pd.concat([total, percent, duplicates], axis=1, keys=['Total', 'Percent', 'Duplicates'])
+
+        print(f'Total and Percentage of NaN & Duplicates:\n {missing_data}')
     else:
-        print('No NaN found.')
+        print('No NaN or Duplicate found.')
