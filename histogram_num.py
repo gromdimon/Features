e25b84cbda3e56408ecf4def73fc9083ee3522ea
Dima Gromyko <grom.dima.grom@gmail.com>
2021-12-15 11:45:50 +0300

    Create histogram_num.py

---
+++ histogram_num.py
@@ -0,0 +1,9 @@
+def histograms_numeric_columns(df, numerical_columns):
+    '''
+    Takes df, numerical columns as list
+    Returns a group of histagrams
+    '''
+    f = pd.melt(df, value_vars=numerical_columns) 
+    g = sns.FacetGrid(f, col='variable',  col_wrap=4, sharex=False, sharey=False)
+    g = g.map(sns.distplot, 'value')
+    return g
