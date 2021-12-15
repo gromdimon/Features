567cd6467dfcbd8ba6f9a4131b01a9e6bea1da46
Dima Gromyko <grom.dima.grom@gmail.com>
2021-12-15 11:55:19 +0300

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -3,3 +3,20 @@ correlations['abs'] = correlations[0].abs()
 sorted_correlations = correlations.sort_values('abs', ascending=False)[0]
 fig, ax = plt.subplots(figsize=(10,20))
 sns.heatmap(sorted_correlations.to_frame(), cmap='coolwarm', annot=True, vmin=-1, vmax=1, ax=ax)
+
+
+# OR
+
+
+def heatmap_numeric_w_dependent_variable(df, dependent_variable):
+    '''
+    Takes df, a dependant variable as str
+    Returns a heatmap of all independent variables' correlations with dependent variable 
+    '''
+    plt.figure(figsize=(8, 10))
+    g = sns.heatmap(df.corr()[[dependent_variable]].sort_values(by=dependent_variable), 
+                    annot=True, 
+                    cmap='coolwarm', 
+                    vmin=-1,
+                    vmax=1) 
+    return g
