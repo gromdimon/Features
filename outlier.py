9292d34f8d9a8a6eeb27dcd29ff2846829a72c1d
Dima Gromyko <grom.dima.grom@gmail.com>
2021-12-16 17:05:26 +0300

    Create outlier.py

---
+++ outlier.py
@@ -0,0 +1,6 @@
+#Dropping the outlier rows with standard deviation
+factor = 3
+upper_lim = data['column'].mean () + data['column'].std () * factor
+lower_lim = data['column'].mean () - data['column'].std () * factor
+
+data = data[(data['column'] < upper_lim) & (data['column'] > lower_lim)]
