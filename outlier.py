2c0f96e5c59ece2cc35cd40a02c5e7db066defdc
Dima Gromyko <grom.dima.grom@gmail.com>
2021-12-16 17:07:41 +0300

    Update outlier.py

--- outlier.py
+++ outlier.py
@@ -4,3 +4,10 @@ upper_lim = data['column'].mean () + data['column'].std () * factor
 lower_lim = data['column'].mean () - data['column'].std () * factor
 
 data = data[(data['column'] < upper_lim) & (data['column'] > lower_lim)]
+
+
+#Dropping the outlier rows with Percentiles
+upper_lim = data['column'].quantile(.95)
+lower_lim = data['column'].quantile(.05)
+
+data = data[(data['column'] < upper_lim) & (data['column'] > lower_lim)]
